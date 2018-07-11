b65844f028d60ac5e99793f2dcf872e4a0c99daa
Yoshi Askharoun <jjask7@gmail.com>
2018-07-11 00:38:19 -0700

    Updated Program.cs for Heroku

--- TextalkREST/Program.cs
+++ TextalkREST/Program.cs
@@ -12,9 +12,23 @@ namespace TextalkREST
 {
     public class Program
     {
-        public static void Main(string[] args)
+        /*public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+        }*/
+
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
